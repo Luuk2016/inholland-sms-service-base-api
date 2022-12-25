1ab45c56e423c821f47fd4250c75bfcf018f4236
Luuk Kenselaar <luuk@lkenselaar.dev>
2022-12-25 22:08:19 +0100

    Return a specific group

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask
+from flask import Flask, jsonify
 import env
-from data.models import db
+from data.models import db, Group
 
 # create the app
 app = Flask(__name__)
@@ -14,10 +14,11 @@ with app.app_context():
     db.create_all()
 
 
-@app.route("/")
-def hello_world():
-    """Return a basic message"""
-    return "<p>Hello, World!</p>"
+@app.route("/group/<uuid:group_id>")
+def get_specific_group(group_id):
+    """Returns a specific group"""
+    group = Group.query.get(group_id)
+    return jsonify(group)
 
 
 if __name__ == "__main__":
