e5ad499624a21cfb861da588fb0c84e31ed0409b
Luuk Kenselaar <luuk@lkenselaar.dev>
2022-12-25 19:19:26 +0100

    Implement SQLAlchemy (#16)
    * WIP: SQLAlchemy
    
    * Add DB info to README.md
    
    * Pylint: ignore env module
    
    * Implement basic models
    
    * Move models from app.py to models.py
    
    * Order of imports?
    
    * Finished SQLAlchemy models
    
    * Remove not needed models
    
    * Update README.md

--- app.py
+++ app.py
@@ -1,9 +1,25 @@
 from flask import Flask
+import env
+from data.models import db
 
+# create the app
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = env.CONNECTION_STRING
+
+# initialize the app with the extension
+db.init_app(app)
+
+# for development only, remove for production
+with app.app_context():
+    db.create_all()
 
 
 @app.route("/")
 def hello_world():
     """Return a basic message"""
     return "<p>Hello, World!</p>"
+
+
+if __name__ == "__main__":
+    print("test")
+    app.run()
