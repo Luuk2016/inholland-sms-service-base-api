a7c2d39cef4786eae43cf8a90e1e819d89d3b838
Luuk Kenselaar <luuk@lkenselaar.dev>
2022-12-25 23:07:45 +0100

    Change the GroupSchema classdoc

--- data/schemes.py
+++ data/schemes.py
@@ -2,6 +2,6 @@ from marshmallow import Schema, fields
 
 
 class GroupSchema(Schema):
-    """The group schema class is used to validate the posted data when trying to create a new group"""
+    """Used to validate the posted data when trying to create a new group"""
     location_id = fields.UUID(required=True)
     name = fields.String(required=True)
